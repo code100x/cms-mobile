b7b5685e11e9da98bd6746e1dd1f26849fa7c714
doppedheart <anuragagarwal530@gmail.com>
2024-10-18 18:04:23 +0530

    Fix: on scroll search box to fix on top

--- app/screens/MyCoursesScreen.tsx
+++ app/screens/MyCoursesScreen.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from "react"
+import React, { FC, useRef } from "react"
 import { observer } from "mobx-react-lite"
-import { TextStyle, View, ViewStyle } from "react-native"
+import { Animated, ScrollView, TextStyle, View, ViewStyle } from "react-native"
 
 import { CourseCard, ListView, Screen, Text, TextField } from "app/components"
-import { spacing, typography } from "app/theme"
+import { colors, spacing, typography } from "app/theme"
 import { TabScreenProps } from "app/navigators"
 
 interface MyCoursesScreenProps extends TabScreenProps<"MyCourses"> {}
@@ -18,29 +18,41 @@ const renderSeparator = () => {
 
 export const MyCoursesScreen: FC<MyCoursesScreenProps> = observer(function MyCoursesScreen() {
   // const { userStore } = useStores()
+  const scrollY = useRef(new Animated.Value(0)).current
   const username = undefined
 
   return (
     <Screen contentContainerStyle={$root} safeAreaEdges={["top"]}>
-      <View>
-        {/* Add user avatar here */}
-        <Text preset="subheading" style={$greeting} text={`Hi, ${username ?? "Hustler"}`} />
-      </View>
-      <Text preset="heading" style={$heading} text="My Courses" />
-      <TextField placeholder="Search videos..." />
-      <ListView
-        // keyExtractor={(item) => item.id}
-        contentContainerStyle={$listContainer}
-        data={[{}, {}, {}, {}, {}]}
-        estimatedItemSize={10}
-        ItemSeparatorComponent={renderSeparator}
-        renderItem={renderCourses}
-        showsVerticalScrollIndicator={false}
-      />
+      <ScrollView
+        stickyHeaderIndices={[2]}
+        scrollEventThrottle={14}
+        onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
+          useNativeDriver: false,
+        })}
+      >
+        <View>
+          {/* Add user avatar here */}
+          <Text preset="subheading" style={$greeting} text={`Hi, ${username ?? "Hustler"}`} />
+        </View>
+        <Text preset="heading" style={$heading} text="My Courses" />
+        <TextField
+          inputWrapperStyle={$searchBox}
+          style={$searchBarWrapper}
+          placeholder="Search videos..."
+        />
+        <ListView
+          // keyExtractor={(item) => item.id}
+          contentContainerStyle={$listContainer}
+          data={[{}, {}, {}, {}, {}]}
+          estimatedItemSize={10}
+          ItemSeparatorComponent={renderSeparator}
+          renderItem={renderCourses}
+          showsVerticalScrollIndicator={false}
+        />
+      </ScrollView>
     </Screen>
   )
 })
-
 const $root: ViewStyle = {
   flex: 1,
   paddingHorizontal: spacing.md,
@@ -60,4 +72,13 @@ const $listContainer: ViewStyle = {
 
 const $separator: ViewStyle = {
   marginVertical: spacing.xs,
+}
+
+const $searchBarWrapper: ViewStyle = {
+  backgroundColor: colors.background.primary,
+}
+
+const $searchBox: ViewStyle = {
+  backgroundColor: colors.background.primary,
+  marginVertical: 10,
 }
