9bfcee95a905bdece142e40dc0385acf235bd0c8
khushal <khushal2004a@gmail.com>
2024-10-18 20:43:44 +0530

    fixed todo: added userStoreDefaults toastStoreDefaults as mentioned in RootStore.ts

--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -1,31 +1,21 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
 import { UserStoreModel } from "./UserStore"
-import { ToastPreset } from "app/constants"
 import { ToastModel } from "./ToastStore"
+import { toastStoreDefaults, userStoreDefaults } from "./helpers/defaultValues"
 
 /**
  * A RootStore model.
  */
 export const RootStoreModel = types.model("RootStore").props({
-  userStore: types.optional(UserStoreModel, {
-    authToken: undefined,
-    email: "",
-    password: "",
-    username: "",
-  }),
-  // TODO: Check how to remove the default values from here and move it in a separate file maybe
-  toastStore: types.optional(ToastModel, {
-    message: null,
-    preset: ToastPreset.Success,
-    showToast: false,
-  }),
+  userStore: types.optional(UserStoreModel, userStoreDefaults),
+  toastStore: types.optional(ToastModel, toastStoreDefaults),
 })
 
 /**
  * The RootStore instance.
  */
-export interface RootStore extends Instance<typeof RootStoreModel> {}
+export interface RootStore extends Instance<typeof RootStoreModel> { }
 /**
  * The data of a RootStore.
  */
-export interface RootStoreSnapshot extends SnapshotOut<typeof RootStoreModel> {}
+export interface RootStoreSnapshot extends SnapshotOut<typeof RootStoreModel> { }
